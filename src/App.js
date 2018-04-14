02ede7bae6fe6e879da663fdcc238ea9d625f0fb
noboru-i <ishikura.noboru@gmail.com>
2018-04-14 13:38:21 +0900

    change base screen

--- src/App.js
+++ src/App.js
@@ -33,21 +33,9 @@ function registerScreens() {
 
 registerScreens();
 
-Navigation.startTabBasedApp({
-  tabs: [
-    {
-      label: 'One',
-      screen: 'example.FirstTabScreen', // this is a registered name for a screen
-      icon: require('../img/one.png'),
-      selectedIcon: require('../img/one_selected.png'), // iOS only
-      title: 'Screen One'
-    },
-    {
-      label: 'Two',
-      screen: 'example.SecondTabScreen',
-      icon: require('../img/two.png'),
-      selectedIcon: require('../img/two_selected.png'), // iOS only
-      title: 'Screen Two'
-    }
-  ]
+Navigation.startSingleScreenApp({
+  screen: {
+    screen: 'example.FirstTabScreen',
+    title: 'Welcome'
+  }
 });
