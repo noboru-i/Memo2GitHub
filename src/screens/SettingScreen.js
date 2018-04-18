1c00f3042d764ace465f48f3db68cad7842a4054
noboru-i <ishikura.noboru@gmail.com>
2018-04-18 22:26:42 +0900

    add repo setting

--- src/screens/SettingScreen.js
+++ src/screens/SettingScreen.js
@@ -17,15 +17,28 @@ export default class extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      token: ''
+      token: '',
+      owner: '',
+      repository: ''
     };
     this.onPressCheck = this.onPressCheck.bind(this);
+    this.onPressApply = this.onPressApply.bind(this);
   }
 
   componentWillMount() {
     AsyncStorage.getItem('auth.token').then(data => {
       if (data) {
         this.setState({ token: data });
+      }
+    });
+    AsyncStorage.getItem('setting.owner').then(data => {
+      if (data) {
+        this.setState({ owner: data });
+      }
+    });
+    AsyncStorage.getItem('setting.repository').then(data => {
+      if (data) {
+        this.setState({ repository: data });
       }
     });
 
@@ -44,10 +57,38 @@ export default class extends React.Component {
       .then(({ data }) => {
         alert(`Hello ${data.login} !!`); // eslint-disable-line no-undef,no-alert
         AsyncStorage.setItem('auth.token', this.state.token);
+        this.setState({ owner: data.login });
       })
       .catch(error => {
         console.log(error);
         alert('Failed to Authenticate. Please recheck token.'); // eslint-disable-line no-undef,no-alert
+      });
+  }
+
+  onPressApply() {
+    const octokit = new GitHubApi({});
+    octokit.authenticate({
+      type: 'token',
+      token: this.state.token
+    });
+    octokit.repos
+      .get({
+        owner: this.state.owner,
+        repo: this.state.repository
+      })
+      .then(({ data }) => {
+        if (!data.issues_url) {
+          alert(`no issue`); // eslint-disable-line no-undef,no-alert
+          return;
+        }
+
+        alert(`has issue`); // eslint-disable-line no-undef,no-alert
+        AsyncStorage.setItem('setting.owner', this.state.owner);
+        AsyncStorage.setItem('setting.repository', this.state.repository);
+      })
+      .catch(error => {
+        console.log(error);
+        alert('Failed to get repository. Please recheck owner/repository.'); // eslint-disable-line no-undef,no-alert
       });
   }
 
@@ -70,7 +111,32 @@ export default class extends React.Component {
             onPress={this.onPressCheck}
             disabled={!this.state.token}
           >
-            <Text>Go to auth</Text>
+            <Text>Check auth</Text>
+          </Button>
+
+          <Form style={{ marginTop: 30 }}>
+            <Item stackedLabel>
+              <Label>owner</Label>
+              <Input
+                onChangeText={text => this.setState({ owner: text })}
+                value={this.state.owner}
+              />
+            </Item>
+            <Item stackedLabel last>
+              <Label>repository</Label>
+              <Input
+                onChangeText={text => this.setState({ repository: text })}
+                value={this.state.repository}
+              />
+            </Item>
+          </Form>
+          <Button
+            block
+            style={{ margin: 10, marginTop: 30 }}
+            onPress={this.onPressApply}
+            disabled={!this.state.owner || !this.state.repository}
+          >
+            <Text>Apply repository</Text>
           </Button>
         </Content>
       </Container>
